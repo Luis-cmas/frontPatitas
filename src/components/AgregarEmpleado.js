be9abed447cf26eaf0a4ca2990a94773d25b132a
Luis-cmas <laacaguilarceja@gmail.com>
2021-12-20 18:08:45 -0600

    actualizacion pestañas de la seccion de empleado

--- src/components/AgregarEmpleado.js
+++ src/components/AgregarEmpleado.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import "./styles/AgregarEmpleado.css"
 export default function AgregarEmpleado() {
     return (
-        <div>
-            <p>agregar empleado</p>
+        <div className='AgregarEmpleado'>
+            
+            <ul className='formEmpleado'>
+                <li className='formEmpleado__item'><p>Nombre</p><input type="text"></input></li>
+                <li className='formEmpleado__item'><p>Primer Apellido</p><input type="text"></input></li>
+                <li className='formEmpleado__item'><p>Segundo Apellido</p><input type="text"></input></li>
+                <li className='formEmpleado__item'><p>Teléfono</p><input type="tel"></input></li>
+                <li className='formEmpleado__item'><p>Especialidad</p><input type="text"></input></li>
+                <li className='formEmpleado__item'><p>Cargo</p><input type="text"></input></li>
+                <li className='formEmpleado__item'><p>Correo</p><input type="email"></input></li>
+                <li className='formEmpleado__item'><p>Contraseña</p><input type="password"></input></li>
+                <li className='formEmpleado__item'><p>Horario de entrada</p><input type="time"></input></li>
+                <li className='formEmpleado__item'><p>Horario de salida</p><input type="time"></input></li>
+            </ul>
         </div>
     )
 }
